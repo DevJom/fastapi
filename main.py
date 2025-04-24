90ee399abfef693f2c9aab3115ac29f548a29059
DevJom <jomarmlazaro@gmail.com>
2025-04-24 21:09:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://deeplungv2.vercel.app",  # Replace with your frontend domain
-        "http://localhost:3000",  # For local development
+        # "https://deeplungv2.vercel.app",  # Replace with your frontend domain
+        # "http://localhost:3000",  # For local development
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
